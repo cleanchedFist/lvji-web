159a62be18de2cce89ea0f61a18ae3d13b8edddf
料事如 <liaoshiru@liaoshirudeMacBook-Pro.local>
2026-05-20 18:02:21 +0800

    fix: 客户上传文档增加字数限制

--- src/pages/ContractCataloge/components/UploadContractModal/index.tsx
+++ src/pages/ContractCataloge/components/UploadContractModal/index.tsx
@@ -36,12 +36,14 @@ const UploadContractModal = forwardRef<UploadContractModalRef>((props, ref) => {
   const { hasFeature } = actions;
 
   const okBtnDisabled = useMemo(() => {
-    return !!(
-      state.orderPrice &&
+    const isTooLong =
+      state.step === 2 && state.contractWordCount && state.contractWordCount > 25000;
+    const isInsufficient =
       state.step === 2 &&
-      +(initialState?.balance?.availableBalance || 0) < +state.orderPrice
-    );
-  }, [initialState?.balance, state.orderPrice, state.step]);
+      state.orderPrice &&
+      +(initialState?.balance?.availableBalance || 0) < +state.orderPrice;
+    return !!(isTooLong || isInsufficient);
+  }, [initialState?.balance, state.orderPrice, state.step, state.contractWordCount]);
 
   // 对外暴露 API
   useImperativeHandle(ref, () => ({
